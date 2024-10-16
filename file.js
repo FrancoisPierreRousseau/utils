dff8cd91eb686764b0f6e2ea6d6bcd696dd82680
François-Pierre <francoispierrerousseau.44@gmail.com>
2024-10-17 00:03:28 +0200

    first commit

---
+++ file.js
@@ -0,0 +1,136 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readdirAsync = util.promisify(fs.readdir)
+
+async function findFilesName(dir){
+    if(typeof dir !== 'string'){
+        throw new Error('Dir doit être de type string')
+    }
+    
+    return (await readdirAsync(dir)).filter(file => !fs.statSync(path.join(dir, file)).isDirectory())
+}
+
+// ( async () => {
+//     console.log(await findFilesName('./'))
+// })()
+
+
+
+async function findSubDirectories(dir){
+    if(typeof dir !== 'string'){
+        throw new Error('Dir doit être de type string')
+    }
+
+    return (await readdirAsync(dir)).map(file => path.join(dir, file)).filter(filePath => fs.statSync(filePath).isDirectory())
+}
+
+// ( async () => {
+//      console.log(await findSubDirectories('./'))
+// })()
+
+async function findSubDirectoriesRecursive(dir){
+
+    if(typeof dir !== 'string'){
+        throw new Error('dir doit être de type string')
+    }
+
+    return (await readdirAsync('./', { recursive: true })).map(file => path.join(dir, file)).filter(filePath => fs.statSync(filePath).isDirectory())
+}
+
+// ( async () => {
+//       console.log(await findSubDirectoriesRecursive('./'))
+// })()
+
+
+async function findFilesRecusive(dir){
+
+    if(typeof dir !== 'string'){
+        throw new Error('dir doit de type string')
+    }
+
+    return (await readdirAsync(dir, {recursive: true})).filter(file => !fs.statSync(path.join(dir, file)).isDirectory())
+}
+
+ // ( async () => {
+ //       console.log(await findFilesRecusive('./'))
+ // })()
+
+
+
+async function findFilesRecursiveByExtension(dir, extension) {
+    if(typeof dir !== 'string'){
+        throw new Error('dir doit de type string')
+    }
+
+    if(typeof extension !== 'string'){
+        throw new Error('extension doit de type string')
+    }
+
+    return (await findFilesRecusive(dir)).filter(filePath => path.extname(filePath) === extension);
+}
+
+// ( async () => {
+//       console.log(await findFilesRecursiveByExtension('./', '.png'))
+// })()
+
+
+
+
+
+// Nom du fichier à supprimer (exemple : 'xxx.png')
+// const fileNameToDelete = 'xxx.png';
+
+// Fonction pour supprimer un fichier
+function deleteFile(filePath) {
+    if (fs.existsSync(filePath)) {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                console.error(`Erreur lors de la suppression du fichier ${filePath}:`, err);
+            } else {
+                console.log(`Fichier supprimé: ${filePath}`);
+            }
+        });
+    }
+}
+
+// Fonction pour parcourir récursivement un répertoire et ses sous-dossiers
+function traverseDirectory(directory) {
+    // Lire le contenu du répertoire
+    fs.readdir(directory, (err, files) => {
+        if (err) {
+            console.error(`Erreur lors de la lecture du dossier ${directory}:`, err);
+            return;
+        }
+
+        // Parcourir chaque fichier/dossier dans le répertoire
+        files.forEach(file => {
+            const filePath = path.join(directory, file);
+
+            // Vérifie si c'est un répertoire
+            fs.stat(filePath, (err, stat) => {
+                if (err) {
+                    console.error(`Erreur lors de la vérification du fichier ${filePath}:`, err);
+                    return;
+                }
+
+                // Si c'est un dossier, on appelle récursivement traverseDirectory
+                if (stat.isDirectory()) {
+                    traverseDirectory(filePath); // Appel récursif
+                } else {
+                    // Si c'est un fichier et qu'il correspond au nom à supprimer
+                    if (file === fileNameToDelete) {
+                        deleteFile(filePath); // Supprimer le fichier
+                    }
+                }
+            });
+        });
+    });
+}
+
+// Répertoire racine où commencer la recherche (remplace-le par le chemin de ton dossier)
+// const rootDirectory = path.resolve(__dirname, 'ton_dossier');
+
+// Lancer la recherche récursive et suppression des fichiers correspondants
+// traverseDirectory(rootDirectory);
