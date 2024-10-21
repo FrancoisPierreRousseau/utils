391f7756f589245d968b6f6fc3e9302036bbe20f
François-Pierre <francoispierrerousseau.44@gmail.com>
2024-10-21 23:59:14 +0200

    dddd

--- files/file.js
+++ files/file.js
@@ -2,31 +2,48 @@ const fs = require('fs');
 const path = require('path');
 const util = require('util');
 
+//
+//  Chaque shéma dans une bdd correspond à un domaine (gestion des callendrier, des dossiers, des droits utilisateurs)
+//  Connecter une API(s) à chaque schéma ce qui permet d'avoir une gestion plus fine au niveau des droits applicatiifs
+//  Lorsqu'un schéma est modifier alors les autres shéma peuvent réagir en conséquence ?   
 
-// dir doit être un tableau de path
 
 const readdirAsync = util.promisify(fs.readdir)
 const unlinkAsync = util.promisify(fs.unlink)
 
-async function findFilesName(dir, { recursive = false  }){
-    if(typeof dir !== 'string'){
-        throw new Error('dir doit être de type string')
+
+
+// dir doit être un tableau de path. limité est conservant uniquement le parent racine, rajouter to: 'ddd' ou s'arrétera la recusivité
+// Puis rajouter un sytéme de filtrage
+async function findFilesName(directories, { recursive = false  }){
+    if(!Array.isArray(directories)){
+        throw new Error('directories doit être un tableau')
     }
 
-    if(!fs.existsSync(dir)){
-        throw new Error(`Le chemin spécifié n'existe pas : ${path.resolve(dir)}`);
+    if(directories.some(directory => typeof directory !== "string")){
+        throw new Error('directories doit être un tableau de string')
     }
 
-    if (!fs.statSync(dir).isDirectory()) {
-        throw new Error(`Le chemin spécifié n'est pas un répertoire valide : ${path.resolve(dir)}`);
+    const resolvedPath = directories.map(directory => path.resolve(directory))
+
+    const unresolvedPath = resolvedPath.filter(directory => !fs.existsSync(directory))
+    if(unresolvedPath.length > 0){
+        throw new Error(`Les chemins spécifiés n'existent pas : ${unresolvedPath.join(', ')}`);
     }
-    
-    return (await readdirAsync(dir, { recursive })).filter(file => !fs.statSync(path.join(dir, file)).isDirectory())
+
+    const filesPath = resolvedPath.filter(directory => !fs.statSync(directory).isDirectory())
+    if (filesPath.length > 0) {
+        throw new Error(`Les chemins spécifiés ne sont pas des répertoires valide : ${filesPath.join(', ')}`);
+    }
+
+
+    const filtreByFilename = async directory => (await readdirAsync(directory, { recursive })).filter(file => !fs.statSync(path.join(directories, file)).isDirectory())
+    return directories.map(async directory => await filtreByFilename(directory) )
 }
 
-// ( async () => {
-//     console.log(await findFilesName('./', { recursive: false }))
-// })()
+( async () => {
+    console.log(await findFilesName(['./', './mem/citation-jacques-chirac-8635.png'] ,{ recursive: false }))
+})()
 
 
 async function findSubDirectories(dir, { recursive = false }){
@@ -118,15 +135,16 @@ async function deleteFileInAllSubdictories(dir, fileName) {
     if (!fs.statSync(dir).isDirectory()) {
         throw new Error(`Le chemin spécifié n'est pas un répertoire valide : ${path.resolve(dir)}`);
     }
+    
 
     // Existe un undefined le supprimer
     const filesPath = (await findFilesName(dir, { recursive: true })).filter(filePath => path.basename(filePath) === fileName)
-
+    
     const filesPathToDelete = [path.join(dir, fileName), ...filesPath].filter(filePath => fs.existsSync(filePath))
     
     return await Promise.all(filesPathToDelete.map(filepath => deleteFile(filepath)))
 }
 
-(async () => {
-     console.log(await deleteFileInAllSubdictories('./','safe.png'))
-})()
+// (async () => {
+//      console.log(await deleteFileInAllSubdictories('./','Ah-ouais-Cest.jpg'))
+// })()
